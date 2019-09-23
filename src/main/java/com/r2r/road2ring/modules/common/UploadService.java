51b74f035a49b24dd8b13b107611f2993caa0e96
GVM-LTP-161\patrick.n.manurung <patrick.n.manurung@gvmnetworks.com>
2019-09-23 14:29:42 +0700

    spaces research done

--- src/main/java/com/r2r/road2ring/modules/common/UploadService.java
+++ src/main/java/com/r2r/road2ring/modules/common/UploadService.java
@@ -3,20 +3,38 @@ package com.r2r.road2ring.modules.common;
 import static com.r2r.road2ring.modules.common.Static.IMAGE_ASSETS;
 import static com.r2r.road2ring.modules.common.Static.SIZE_100KB;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.services.s3.transfer.model.UploadResult;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Set;
+import java.util.TimeZone;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.tomcat.util.http.fileupload.FileUploadBase.FileSizeLimitExceededException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,144 +46,70 @@ public class UploadService {
   @Value("${r2r.upload.path}")
   String imagePath;
 
+  @Value("${r2r.spaces.full-link}")
+  String fullLink;
+
   @Autowired
-  public void setR2rTools(R2rTools r2rTools) {
-    this.r2rTools = r2rTools;
-  }
+  AmazonS3 s3Client;
+
+  @Autowired
+  Environment env;
+
+  @Value("${r2r.bucket.name}")
+  String S3_BUCKET_NAME;
+
+
+  @Value("${r2r.picfolder.name}")
+  String FOLDER;
 
   @Autowired
   private HttpServletRequest request;
 
+  @Autowired
+  public void setR2rTools(R2rTools r2rTools) {
+    this.r2rTools = r2rTools;
+  }
   public String uploadImagePicture(MultipartFile file, String typeImage)
       throws IOException, FileSizeLimitExceededException {
-    String linkUrl;
+
+    String linkUrl = "";
     String type = file.getContentType().split("/")[1];
     Long date = new Date().getTime();
-
-    byte[] bytes;
 
     if (file.getSize() > SIZE_100KB) {
       throw new FileSizeLimitExceededException("File is too Big", file.getSize(), SIZE_100KB);
     }
-    linkUrl = date + r2rTools.generateRandomCode(8) + "." + type;
+    try {
+      String picName = date + r2rTools.generateRandomCode(8);
+      linkUrl = fullLink + FOLDER + picName + "." + type;
+      File convFile = convertMultiPartToFile(file);
+      this.uploadToSpaces(convFile, picName, type);
+      convFile.delete();
+    } catch (Exception e){
+      e.printStackTrace();
+    }
 
-    String pathtoUploads = request.getServletContext().getRealPath("/uploads/");
-
-    String pathImageWebApp = pathtoUploads + linkUrl;
-    File picture = new File(pathImageWebApp);
-    file.transferTo(picture);
-
-    this.uploadImagePicture(linkUrl);
-    this.deleteBaseImage(pathImageWebApp);
 
     return linkUrl;
 
   }
 
-  private void deleteBaseImage(String name) {
-    File file = new File(name);
-    file.delete();
+  public void uploadToSpaces(File file, String imgName, String ext) throws IOException {
+    String key = FOLDER + imgName + "." + ext;
+    s3Client.putObject(new PutObjectRequest(S3_BUCKET_NAME, key, file)
+        .withCannedAcl(CannedAccessControlList.PublicRead));
   }
 
-  private void uploadImagePicture(String name)
-      throws IOException {
-    String pathtoUploads = request.getServletContext().getRealPath("/uploads/");
-
-    File inputFile = new File(pathtoUploads + name);
-    BufferedImage inputImage = ImageIO.read(inputFile);
-
-    String outputImagePath = IMAGE_ASSETS + name;
-
-    // creates output image
-    BufferedImage outputImage = new BufferedImage(inputImage.getWidth(),
-        inputImage.getHeight(), inputImage.getType());
-
-    // scales the input image to the output image
-    Graphics2D g2d = outputImage.createGraphics();
-    g2d.drawImage(inputImage, 0, 0, inputImage.getWidth(), inputImage.getHeight(), null);
-    g2d.dispose();
-
-    // extracts extension of output file
-    String formatName = outputImagePath.substring(outputImagePath
-        .lastIndexOf(".") + 1);
-
-    // writes to output file
-    File editedFile = new File(outputImagePath);
-    ImageIO.write(outputImage, formatName, editedFile);
-    if (editedFile.exists()) {
-//	  editedFile.setExecutable(true,false);
-      editedFile.setReadable(true, false);
-//    editedFile.setWritable(true,false);
-    }
-//    File outFile = new File(outputImagePath);
-//    ImageIO.write(outputImage, formatName, outFile);
-//    Set<PosixFilePermission> ownerWritable = PosixFilePermissions.fromString("rw-r--r--");
-//    Files.setPosixFilePermissions(outFile.toPath(),ownerWritable);
+  public void deleteFile(String fileName) throws Exception {
+    s3Client.deleteObject(new DeleteObjectRequest(S3_BUCKET_NAME, FOLDER + fileName));
   }
 
-  public String uploadIconPicture(MultipartFile file, String typeImage)
-      throws IOException, FileSizeLimitExceededException {
-    String linkUrl;
-    String type = file.getContentType().split("/")[1];
-    Long date = new Date().getTime();
-
-    byte[] bytes;
-
-    if (file.getSize() > SIZE_100KB) {
-      throw new FileSizeLimitExceededException("File is too Big", file.getSize(), SIZE_100KB);
-    }
-    linkUrl = date + r2rTools.generateRandomCode(8) + "." + type;
-
-    String pathtoUploads = request.getServletContext().getRealPath("/uploads/");
-
-    String pathImageWebApp = pathtoUploads + linkUrl;
-
-    File picture = new File(pathImageWebApp);
-//    bytes = file.getBytes();
-//
-//    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(picture));
-//    stream.write(bytes);
-//    stream.close();
-    file.transferTo(picture);
-
-    this.uploadIconPicture(linkUrl);
-    this.deleteBaseImage(pathImageWebApp);
-    return linkUrl;
-  }
-
-  private void uploadIconPicture(String name) throws IOException {
-    String pathtoUploads = request.getServletContext().getRealPath("/uploads/");
-
-    File inputFile = new File(pathtoUploads + name);
-    BufferedImage inputImage = ImageIO.read(inputFile);
-    String outputImagePath = IMAGE_ASSETS + name;
-
-    // creates output image
-    BufferedImage outputImage = new BufferedImage(inputImage.getWidth(),
-        inputImage.getHeight(), inputImage.getType());
-
-    // scales the input image to the output image
-    Graphics2D g2d = outputImage.createGraphics();
-    g2d.drawImage(inputImage, 0, 0, inputImage.getWidth(), inputImage.getHeight(), null);
-    g2d.dispose();
-
-    // extracts extension of output file
-    String formatName = outputImagePath.substring(outputImagePath
-        .lastIndexOf(".") + 1);
-
-    // writes to output file
-//    ImageIO.write(outputImage, formatName, new File(outputImagePath));
-
-    File editedFile = new File(outputImagePath);
-    ImageIO.write(outputImage, formatName, editedFile);
-    if (editedFile.exists()) {
-      editedFile.setReadable(true, false);
-    }
-
-//    File outFile = new File(outputImagePath);
-//    ImageIO.write(outputImage, formatName, outFile);
-//    Set<PosixFilePermission> ownerWritable = PosixFilePermissions.fromString("rw-r--r--");
-//    Files.setPosixFilePermissions(outFile.toPath(),ownerWritable);
+  private File convertMultiPartToFile(MultipartFile file) throws IOException {
+    File convFile = new File(file.getOriginalFilename());
+    FileOutputStream fos = new FileOutputStream(convFile);
+    fos.write(file.getBytes());
+    fos.close();
+    return convFile;
   }
 
 }
--- src/main/java/com/r2r/road2ring/modules/trip/TripAPIController.java
+++ src/main/java/com/r2r/road2ring/modules/trip/TripAPIController.java
@@ -113,24 +113,6 @@ public class TripAPIController {
     return responseMessage;
   }
 
-  @PostMapping("/upload_icon/{typeImage}")
-  public ResponseMessage uploadIcon(@PathVariable String typeImage,@RequestParam("file") MultipartFile file) {
-    ResponseMessage responseMessage = new ResponseMessage();
-    try {
-      if (!file.isEmpty()) {
-        responseMessage.setCode(ResponseCode.SUCCESS.getCode());
-        responseMessage.setObject(uploadService.uploadIconPicture(file, typeImage));
-      }
-    }catch (IOException e){
-      responseMessage.setMessage(e.getMessage());
-      responseMessage.setCode(ResponseCode.INTERNAL_SERVER_ERROR.getCode());
-    }catch (FileSizeLimitExceededException e){
-      responseMessage.setMessage(e.getMessage());
-      responseMessage.setCode(ResponseCode.INTERNAL_SERVER_ERROR.getCode());
-    }
-    return responseMessage;
-  }
-
   @PostMapping("/change-status/{tripId}/{statusId}")
   public ResponseMessage changeStatusTrip(@PathVariable("tripId") int tripId,
       @PathVariable("statusId") TripPublishedStatus statusId, Principal principal){
